ad4438b6a5a3155d70da214bf4e004d54387d8fb
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-05-23 16:09:25 -0300

    Added automatic login/logout
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_wikilegis/apps.py
+++ src/colab_wikilegis/apps.py
@@ -1,5 +1,6 @@
-
 from colab.plugins.utils.apps import ColabPluginAppConfig
+from colab_wikilegis.tasks import login_user, logout_user
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
 class WikilegisAppConfig(ColabPluginAppConfig):
@@ -7,3 +8,7 @@ class WikilegisAppConfig(ColabPluginAppConfig):
     verbose_name = 'Colab Wikilegis Plugin'
     short_name = 'wikilegis'
     namespace = 'wikilegis'
+
+    def connect_signal(self):
+        user_logged_in.connect(login_user)
+        user_logged_out.connect(logout_user)
