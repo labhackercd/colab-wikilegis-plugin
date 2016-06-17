fc4497d3d5b45775e35a85963792d8b807302f9f
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-06-17 19:25:15 -0300

    Importing user from wikilegis
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_wikilegis/management/commands/import_wikilegis_users.py
+++ src/colab_wikilegis/management/commands/import_wikilegis_users.py
@@ -5,7 +5,6 @@ from django.template.loader import render_to_string
 from colab_wikilegis.data_importer import ColabWikilegisPluginDataImporter
 import uuid
 
-
 User = get_user_model()
 
 
@@ -14,17 +13,25 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         importer = ColabWikilegisPluginDataImporter()
         users = importer.fetch_users()
+        colab_users = User.objects.all()
         for user in users:
+            username_in_db = colab_users.filter(username=user.username)
+            username_in_db = username_in_db.exclude(email=user.email).count()
+            if username_in_db:
+                user.username = user.username + str(username_in_db)
+
             new_password = str(uuid.uuid4().get_hex()[0:10])
-            print user.username, new_password
-            user.set_password(new_password)
             new_user, created = User.objects.get_or_create(
                 username=user.username,
                 email=user.email,
+                first_name=user.first_name,
+                last_name=user.last_name
             )
             new_user.set_password(new_password)
             new_user.is_active = True
             new_user.save()
+
+            print "Importing " + new_user.username
 
             if created:
                 self.send_email(new_user, new_password)
