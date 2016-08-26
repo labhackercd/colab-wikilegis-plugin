6678d3975688b2279956e2d724b6dbe92a36a9f4
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-26 19:46:39 -0300

    Rewrited wikilegis login url
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_wikilegis/views.py
+++ src/colab_wikilegis/views.py
@@ -1,10 +1,13 @@
-
+from django.conf import settings
 from colab.plugins.views import ColabProxyView
 
 
 class ColabWikilegisPluginProxyView(ColabProxyView):
     app_label = 'colab_wikilegis'
     diazo_theme_template = 'proxy/wikilegis.html'
+    rewrite = (
+        (r'^/wikilegis/accounts/login/?$', r'{}'.format(settings.LOGIN_URL)),
+    )
 
     def get_proxy_request_headers(self, request):
         headers = super(ColabWikilegisPluginProxyView,
