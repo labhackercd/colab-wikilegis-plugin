3427bfebacf59d6754e994204580d71a971713bd
erivanio <erivanio.vanin@gmail.com>
2017-03-28 11:12:26 -0300

    add wikilegis urls to override in colab

--- src/colab_wikilegis/views.py
+++ src/colab_wikilegis/views.py
@@ -7,7 +7,18 @@ class ColabWikilegisPluginProxyView(ColabProxyView):
     diazo_theme_template = 'proxy/wikilegis.html'
     rewrite = (
         (r'^/wikilegis/accounts/login/?$', r'{}'.format(settings.LOGIN_URL)),
-        (r'^/wikilegis/accounts/logout(.*)$', r'{}\1'.format('/account/logout')),
+        (r'^/wikilegis/accounts/register/?$',
+            r'{}'.format(settings.LOGIN_URL)),
+        (r'^/wikilegis/accounts/password_reset/?$',
+            r'{}'.format('/account/password-reset')),
+        (r'^/wikilegis/accounts/password_reset/done/?$',
+            r'{}'.format('/home')),
+        (r'^/wikilegis/accounts/password_change/?$',
+            r'{}'.format('/password_change')),
+        (r'^/wikilegis/accounts/password_change/done/?$',
+            r'{}'.format('/password_change/done')),
+        (r'^/wikilegis/accounts/logout(.*)$',
+            r'{}\1'.format('/account/logout')),
         (r'^/wikilegis/widget/login(.*)$', r'{}\1'.format('/widget/login')),
         (r'^/wikilegis/widget/signup(.*)$', r'{}\1'.format('/widget/signup')),
     )
