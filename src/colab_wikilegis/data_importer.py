4e36aab6481ba09f4a8a536250eeb47ee29b32a9
erivanio <erivanio.vanin@gmail.com>
2017-05-29 17:30:15 -0300

    fix path when have next url

--- src/colab_wikilegis/data_importer.py
+++ src/colab_wikilegis/data_importer.py
@@ -5,7 +5,6 @@ from colab.plugins.data import PluginDataImporter
 from django.utils.dateparse import parse_datetime
 from django.contrib.auth import get_user_model
 from colab_wikilegis import models
-from 
 import requests
 import urllib
 import logging
